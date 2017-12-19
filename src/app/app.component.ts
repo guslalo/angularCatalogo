b73430cbe436382bc07ffa7896215ee0dfc6b2aa
guslalo <gf.renjifo@gmail.com>
2017-12-19 14:09:03 -0300

    modal equipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,18 @@ declare var $:any;//declaramos variable jquery
   templateUrl: './app.component.html',
   styleUrls: ['./../assets/sass/materialize.scss'],
   providers:[GeneralService]
-  
+
 })
 
 
 export class AppComponent  {
-  
-  equiposApi = 'https://manager.rinnolab.cl/catalogue/api/equipment/'; 
+  equiposApi = 'https://manager.rinnolab.cl/catalogue/api/equipment/';
   proveedoresApi = 'https://manager.rinnolab.cl/catalogue/api/provider/';
   detealleEquipoApi = "https://manager.rinnolab.cl/catalogue/api/equipmentdetail/";
   stickerApi = "https://manager.rinnolab.cl/catalogue/api/sticker/";
   equipos: Array<Equipo>;
   equiposActivos: Array<Equipo>;
+  equipoId: Array<Equipo>;
   equipoDetalle:Array<detalleEquipo>;
   stickerm:Array<stickerm>;
   marcaproveedor:Array<proveedormarca>;
@@ -32,101 +32,70 @@ export class AppComponent  {
 
 
   //stickersa:Array<stickerm>;
-  ngOnInit() {  
-    
+  ngOnInit() {
+
   };
 
   constructor(private http:Http, ){//servicio:GeneralService
-      this.getEquipos();
-      this.getProveedor();
-      this.getEquipoDetalle();
-      //servicio.getcoloresEquipos();
-      //this.getSticker();  
-      //this.getcoloresEquipos(); 
+    this.getEquipos();
+    this.getProveedor();
+    this.getEquipoDetalle();
+    this.getSticker();
 
+  }
 
-
-      
-  } 
-  
-
- /* getcoloresEquipos(){
-    this.http.get("https://manager.rinnolab.cl/catalogue/api/colorequipament/").subscribe((resp2) =>{ 
-      this.coloresEquipo = resp2.json().results;   
-    });
-  }*/
 
   //Traer equipos
   getEquipos(){
-    this.http.get(this.equiposApi).subscribe((resp) =>{    
-      this.equipos = resp.json().results; 
-      this.equiposActivos = this.equipos.filter(r => r.is_active == true);  
-     
-        for (let item of this.equiposActivos){
-          
-          /*var objetocolor = item.colors;
-          for (let item of objetocolor){
-            console.log(item.name)
-            item.color = item.name;
-          }*/
-          
-          /*var color:Array<color> = this.coloresEquipo.filter(c => c.equipment == item.id);
-          item.colorEquipo = color[0].color;*/
+    this.http.get(this.equiposApi).subscribe((resp) =>{
+      this.equipos = resp.json().results;
+      this.equiposActivos = this.equipos.filter(r => r.is_active == true);
 
-         
+
+
+        for (let item of this.equiposActivos){
+
           //compara id de provider en equipiactivo y id en proveedores
            var marca:Array<proveedormarca> = this.marcaproveedor.filter(m => m.id == item.provider);
            item.providerName = marca[0].name;
-         
-          
+
             //trae precio prepago equipo
             var equipoDetalle:Array<detalleEquipo> = this.equipoDetalle.filter(me => me.equipment == item.id);
             item.precio = equipoDetalle[0].sale_price;
 
-           /* var stickerm:Array<stickerm> = this.stickerm.filter(ms => ms.id == item.sticker);   
-            item.logoRed = stickerm[0].image;*/
-  
+            /*var stickerm:Array<stickerm> = this.stickerm.filter(ms => ms.id == item.sticker);
+            item.logoRed = stickerm[0].image; */
+
         };
     });
   }
- 
-  getEquipoId(){
-    for (let item of this.equiposActivos){
-        /*if( obtenerId(id) == item.id){
-          
-        }*/
-    }
-    
-    /*
-    this.http.get(this.detealleEquipoApi).subscribe((resp2) =>{ 
-      this.equipoDetalle = resp2.json().results; 
-    });*/
+
+  obtenerId(idSeleccionado){
+    //return idSeleccionado;
+    console.log(idSeleccionado);
+    this.equipoId = this.equipos.filter(r => r.id == idSeleccionado);
   }
 
   //Trae detalle equipo
   getEquipoDetalle(){
-    this.http.get(this.detealleEquipoApi).subscribe((resp2) =>{ 
-      this.equipoDetalle = resp2.json().results; 
+    this.http.get(this.detealleEquipoApi).subscribe((resp2) =>{
+      this.equipoDetalle = resp2.json().results;
     });
   }
 
   getSticker(){
-    this.http.get(this.stickerApi).subscribe((resp) =>{ 
+    this.http.get(this.stickerApi).subscribe((resp) =>{
       this.stickerm = resp.json().results;
-     /* for (let item of this.stickersa){ 
+     /* for (let item of this.stickersa){
         console.log(item);
       }*/
     });
   }
   //Traer proveedor
   getProveedor(){
-    this.http.get(this.proveedoresApi).subscribe((resp2) =>{ 
-      this.marcaproveedor = resp2.json().results;    
+    this.http.get(this.proveedoresApi).subscribe((resp2) =>{
+      this.marcaproveedor = resp2.json().results;
     });
-  }
-
-   obtenerId(id){
-    console.log(id); 
   }
 
 }
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -6,36 +6,7 @@ import { Component, OnInit } from '@angular/core';
   /*template:`
     <div id="modal" class="modal">
       <a href="#!" class="modal-action modal-close waves-effect close"><i class="material-icons">&#xE5CD;</i></a>
-      <div class="modal-content">
-        <div class="row ficha">
-          <div class="col s12 m6">
-            <div class="img">
-              <img class="imgm" src="">
-            </div>
-          </div>
-          <div class="col s12 m6">
-            <div class="m marca"></div>
-            <div class="m nombre"></div>
-        
-            <div class="m detalle">
-              
-            </div>
-    
-            <div class="boxprecios">   
-                <div>
-                  <span class="titulo">Precio Normal</span>
-                  <span class="m valor dos"></span>
-                </div>
-                <div>
-                  <span class="m titulo">Cliente Entel</span>
-                  <span class="m valor ent"></span>
-                </div>
-              </div>
-              <p class="titulo">Equipos compatibles:</p>
-              <div class="carouselbox"></div>    
-          </div>
-        </div>
-      </div> 
+  
     </div>`,*/
   styleUrls: ['./modal.component.scss']
 })
